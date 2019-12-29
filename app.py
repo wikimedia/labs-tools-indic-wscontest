52def0081a9d674bc42fd577256a243bd2122ace
Jayprakash12345 <0freerunning@gmail.com>
2019-12-30 03:05:01 +0530

    Add OAuth support
    Bug: T241537
    Change-Id: Ifcc2224991f5a29653077cec1a17ea7ed1d17eaf

--- app.py
+++ app.py
@@ -1,9 +1,21 @@
-from flask import Flask, request, redirect, render_template
+from flask import Flask, request, redirect, render_template, \
+    session, url_for
 import os
+import mwoauth
 
 # Create the app
 app = Flask(__name__)
+
+# Load configuration
+# export APP_SETTINGS = "config.production" for Production
+app.config.from_object(os.environ['APP_SETTINGS'])
 app.secret_key = os.urandom(50)
+
+consumer_token = mwoauth.ConsumerToken(
+    app.config["CONSUMER_KEY"],
+    app.config["CONSUMER_SECRET"]
+)
+handshaker = mwoauth.Handshaker(app.config["OAUTH_MWURI"], consumer_token)
 
 
 @app.route('/')
@@ -31,6 +43,64 @@ def edit_contest(id):
     return render_template("edit_contest.html")
 
 
+@app.route('/login')
+def login():
+    redirect_to, request_token = handshaker.initiate()
+    keyed_token_name = _str(request_token.key) + '_request_token'
+    keyed_next_name = _str(request_token.key) + '_next'
+    session[keyed_token_name] = \
+        dict(zip(request_token._fields, request_token))
+
+    if 'next' in request.args:
+        session[keyed_next_name] = request.args.get('next')
+    else:
+        session[keyed_next_name] = 'index'
+
+    return redirect(redirect_to)
+
+
+@app.route('/logout')
+def logout():
+    session['mwoauth_access_token'] = None
+    session['mwoauth_username'] = None
+    if 'next' in request.args:
+        return redirect(request.args['next'])
+    return redirect(url_for('index'))
+
+
+@app.route('/oauth-callback')
+def oauth_callback():
+    request_token_key = request.args.get('oauth_token', 'None')
+    keyed_token_name = _str(request_token_key) + '_request_token'
+    keyed_next_name = _str(request_token_key) + '_next'
+
+    if keyed_token_name not in session:
+        err_msg = "OAuth callback failed. Can't find keyed token. Are cookies disabled?"
+        return render_template("error.html", msg=err_msg)
+
+    access_token = handshaker.complete(
+        mwoauth.RequestToken(**session[keyed_token_name]),
+        request.query_string)
+    session['mwoauth_access_token'] = \
+        dict(zip(access_token._fields, access_token))
+
+    next_url = url_for(session[keyed_next_name])
+    del session[keyed_next_name]
+    del session[keyed_token_name]
+
+    get_current_user(False)
+
+    return redirect(next_url)
+
+
+@app.context_processor
+def inject_base_variables():
+    return {
+        "logged": get_current_user() is not None,
+        "username": get_current_user()
+    }
+
+
 @app.before_request
 def force_https():
     if request.headers.get('X-Forwarded-Proto') == 'http':
@@ -38,6 +108,37 @@ def force_https():
             'https://' + request.headers['Host'] + request.headers['X-Original-URI'],
             code=301
         )
+
+
+def get_current_user(cached=True):
+    if cached:
+        return session.get('mwoauth_username')
+
+    # Get user info
+    identity = handshaker.identify(
+        mwoauth.AccessToken(**session['mwoauth_access_token']))
+
+    # Store user info in session
+    session['mwoauth_username'] = identity['username']
+    session['mwoauth_useremail'] = identity['email']
+
+    return session['mwoauth_username']
+
+
+def _str(val):
+    """
+    Ensures that the val is the default str() type for python2 or 3
+    """
+    if str == bytes:
+        if isinstance(val, str):
+            return val
+        else:
+            return str(val)
+    else:
+        if isinstance(val, str):
+            return val
+        else:
+            return str(val, 'ascii')
 
 
 if __name__ == '__main__':
---
+++ config.py
@@ -0,0 +1,27 @@
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
+class config(object):
+    DEBUG = False
+    TESTING = False
+
+
+class production(config):
+    DEBUG = False
+    CONSUMER_KEY = ""
+    CONSUMER_SECRET = ""
+    OAUTH_MWURI = "https://meta.wikimedia.org/w"
+    SESSION_COOKIE_NAME = 'indic-wscontest'
+    SESSION_COOKIE_PATH = '/indic-wscontest'
+    SESSION_COOKIE_SECURE = True
+    SESSION_REFRESH_EACH_REQUEST = False
+    PREFERRED_URL_SCHEME = 'https'
+
+
+class local(config):
+    DEVELOPMENT = True
+    DEBUG = True
+    CONSUMER_KEY = "c13c308a51b8129eccc88e1509c97cae"
+    CONSUMER_SECRET = "134341041cfc0083ca509492fab247d91b6e58a9"
+    OAUTH_MWURI = "http://192.168.64.3"
