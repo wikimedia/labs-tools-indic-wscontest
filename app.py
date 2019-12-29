3868df78a557c554ce71e579402cc4ff35abfb6e
Jayprakash12345 <0freerunning@gmail.com>
2019-12-29 23:18:02 +0530

    Add basic route structure
    Bug: T241537
    Change-Id: I4f95094c47ea60af739abe8a4a33e3d6824348ef

---
+++ app.py
@@ -0,0 +1,43 @@
+from flask import Flask, request, redirect, render_template
+import os
+
+# Create the app
+app = Flask(__name__)
+app.secret_key = os.urandom(50)
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+
+@app.route('/contest/', methods=["GET"])
+def contest():
+    return render_template("contests.html")
+
+
+@app.route('/contest/create', methods=["GET", "POST"])
+def create_contest():
+    return render_template("create_contest.html")
+
+
+@app.route('/contest/<int:id>', methods=["GET"])
+def contest_by_id(id):
+    return render_template("contest.html")
+
+
+@app.route('/contest/<int:id>/edit', methods=["GET", "POST"])
+def edit_contest(id):
+    return render_template("edit_contest.html")
+
+
+@app.before_request
+def force_https():
+    if request.headers.get('X-Forwarded-Proto') == 'http':
+        return redirect(
+            'https://' + request.headers['Host'] + request.headers['X-Original-URI'],
+            code=301
+        )
+
+
+if __name__ == '__main__':
+    app.run()
